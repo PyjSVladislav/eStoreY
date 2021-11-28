b664ab30a165df1088ced30e465e96d08f8a0a42
Vladislav Makarevich <pyjsvladislav@gmail.com>
2021-11-29 01:07:35 +0300

    Adding the quantity of product to the seccion

--- basket/basket.py
+++ basket/basket.py
@@ -13,7 +13,7 @@ class Basket():
         self.basket = basket 
 
 
-    def add(self, product):
+    def add(self, product, qty):
         '''
         Adding and updating the users basket session data
         '''
@@ -21,6 +21,9 @@ class Basket():
         product_id = product.id
         
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': float(product.price)}
+            self.basket[product_id] = {'price': float(product.price), 'qty': int(qty)}
         
         self.session.modified = True
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())
