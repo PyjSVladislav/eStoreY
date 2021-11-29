c6d483a81388c4fe108c545d324047e5cd046203
Vladislav Makarevich <pyjsvladislav@gmail.com>
2021-11-29 16:51:34 +0300

    Create the basket summary template

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from store.models import Product
+from decimal import Decimal, getcontext
+
 class Basket():
     '''
     A base Basket class, providing some default behaviours that
@@ -21,9 +24,36 @@ class Basket():
         product_id = product.id
         
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': float(product.price), 'qty': int(qty)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
         
         self.session.modified = True
 
+
+    def __iter__(self):
+        '''
+        Collect the product_id in the session data to query the database 
+        and return products
+        '''
+
+        products_ids = self.basket.keys()
+        products = Product.objects.filter(id__in=products_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+        
+        for item in basket.values():
+            getcontext().prec = 4
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item 
+
     def __len__(self):
+        '''
+        Get the basket data and count the qty of items
+        '''
         return sum(item['qty'] for item in self.basket.values())
+
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
